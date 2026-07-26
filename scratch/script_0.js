4fa7665d8a840fe64310f8e6664f46ac69edfc75
gripet43 <gripet43@users.noreply.github.com>
2026-07-26 17:32:16 +0800

    Replace BGM track with user provided sleep music (verclub_music-sleep-music-571041.mp3)

---
+++ scratch/script_0.js
@@ -0,0 +1,23 @@
+
+  {
+    "@context": "https://schema.org",
+    "@type": "WebApplication",
+    "name": "推开世界的窗",
+    "alternateName": "Open World Window",
+    "url": "https://gripet43.github.io/breathe-window/",
+    "description": "推开世界的窗是一款温暖治愈的全球城市风情水彩画册与探索应用，带你在书桌前推开 300 个世界城市的木窗，领略精致手绘水彩插画、人文故事与自然风光。",
+    "applicationCategory": "EntertainmentApplication",
+    "operatingSystem": "All",
+    "inLanguage": "zh-CN",
+    "author": {
+      "@type": "Organization",
+      "name": "推开世界的窗团队"
+    },
+    "image": "https://gripet43.github.io/breathe-window/assets/images/world_corner.png",
+    "offers": {
+      "@type": "Offer",
+      "price": "0",
+      "priceCurrency": "USD"
+    }
+  }
+  
